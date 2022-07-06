d2379ebf39861fc795b7b9a5cc73368f1cf1fa2a
Muhammad Hammad Khan <104036712+hammadkhan2930029@users.noreply.github.com>
2022-07-06 07:58:16 +0500

    Add files via upload

---
+++ javascript/chapter 31-34/app.js
@@ -0,0 +1,15 @@
+// chapter 31-34
+
+// task 1
+
+var currDate = new Date();
+
+document.write("Queation no 1 :" +currDate + "<br>");
+
+
+// task 2
+
+var currMonth = new Date ().getMonth();
+var month = ["jan","feb","mar","apr","may","jun","july","aug","sep","oct","nov","dec"]
+
+document.write("Queation no 2 :" + currMonth +" "+ month[currMonth] + "<br>")
